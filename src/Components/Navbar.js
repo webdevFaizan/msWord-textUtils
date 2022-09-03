82c75c55155e223c4872c51d7ca6c81e3f9fd19b
webdevFaizan <webdev.faizan@gmail.com>
2022-09-04 01:51:39 +0530

    Some basic name changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(props) {
   return (
     <div>
         <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <Link className="navbar-brand" to="/">TextUtils</Link>
+        <Link className="navbar-brand" to="/">MSWord-Text Utility</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
